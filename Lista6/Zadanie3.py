410ec23f5ae06b1f2736c30a2e6c825fa9b6e66f
KarlosReaper <65513906+KarlosReaper@users.noreply.github.com>
2022-11-30 22:09:10 +0100

    Add files via upload

--- Lista6/Zadanie3.py
+++ Lista6/Zadanie3.py
@@ -4,36 +4,50 @@
 
 print('Zadanie 3')
 
-liczby = ['zera','jedynki','dwojki','trojki','czworki','piatki','szostki','siodemki','osemki','dziewiatki']
 cyfry = ['0','1','2','3','4','5','6','7','8','9']
 
-a = input('Podaj ciag liczb do przetworzenia: ')
-dlugosc = len(a)
+a = int(input('Podaj liczbe do utworzenia ciagu: '))
 
-j = 0
-li = []
-ni = []
-for i in range(dlugosc):
-    if a[i] in cyfry and i == 0:
+def look(a):
+    a = str(a)
+    dlugosc = len(a)
+    j = 0
+    li = []
+    ni = []
+    f = ''
+    if dlugosc == 1:
+        i = 0
         b = a[i]
         j = j+1
-    elif a[i] in cyfry and i > 0:
-        if b == a[i]:
-            j = j+1
-            if i == dlugosc-1:
-                li = li+[b]
-                ni = ni+[j]
-        else:
-            li = li+[b]
-            ni = ni+[j]
-            b = a[i]
-            j = 1
-            if i == dlugosc-1:
-                li = li+[b]
-                ni = ni+[j]
-    
+        li = li+[b]
+        ni = ni+[j]
+        f=f+str(ni[i])+str(cyfry[int(li[i])])
+    else:
+        for i in range(dlugosc):
+            if a[i] in cyfry and i == 0:
+                b = a[i]
+                j = j+1
+            elif a[i] in cyfry and i > 0:
+                if b == a[i]:
+                    j = j+1
+                    if i == dlugosc-1:
+                        li = li+[b]
+                        ni = ni+[j]
+                else:
+                    li = li+[b]
+                    ni = ni+[j]
+                    b = a[i]
+                    j = 1
+                    if i == dlugosc-1:
+                        li = li+[b]
+                        ni = ni+[j]
+        for i in range(len(li)):
+            f=f+str(ni[i])+str(cyfry[int(li[i])])
+    return f
 
-print(li)
-print(ni)
-for i in range(len(li)):
-    print(ni[i], liczby[int(li[i])], end=" ")
+c = a
+print(a)
+for i in range(c):
+    g = look(a)
+    print(g)
+    a = g
